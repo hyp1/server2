5aa9f83bf8a7c8897d5ae36cf2f73e453e9d1468
robert <robert_schweng@hotmail.com>
2018-08-04 17:55:28 +0200

    10th

--- server.js
+++ server.js
@@ -26,10 +26,37 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
   mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
   mongoURLLabel = "";
+  if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+    mongoUser = process.env[mongoServiceName + '_USER'];
+  
+    if (mongoHost && mongoPort && mongoDatabase) {
+      mongoURLLabel = mongoURL = 'mongodb://';
+      if (mongoUser && mongoPassword) {
+        mongoURL += mongoUser + ':' + mongoPassword + '@';
+      }
+      // Provide UI label that excludes user id and pw
+      mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+      mongoURL += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+  
+    }
+  }
 
-
-console.log("RUNNING");
+console.log(mongoURL, 'MONGO VORHER');
 console.log(process.env.MONGO_TEST,"MONGOTEST");
+
+var db = null;
+function initDB() {
+  instance.setup(mongoURL);
+  db = instance.DbConnection;
+  if (!db) console.error('Keine Datenbank!')
+}
+initDB();
+console.log(db,"DB");
 
 
 var http = require('http').Server(app);
